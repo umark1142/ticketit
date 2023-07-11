fc0cfc175bc312525f777dafef262a194cce4dac
umark1142 <53573755+umark1142@users.noreply.github.com>
2023-07-11 20:57:16 +0500

    first commit

---
+++ src/Controllers/CategoriesController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Kordy\Ticketit\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Kordy\Ticketit\Models\Category;
+use Kordy\Ticketit\Helpers\LaravelVersion;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        // seconds expected for L5.8<=, minutes before that
+        $time = LaravelVersion::min('5.8') ? 60*60 : 60;
+        $categories = \Cache::remember('ticketit::categories', $time, function () {
+            return Category::all();
+        });
+
+        return view('ticketit::admin.category.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('ticketit::admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      => 'required',
+            'color'     => 'required',
+        ]);
+
+        $category = new Category();
+        $category->create(['name' => $request->name, 'color' => $request->color]);
+
+        Session::flash('status', trans('ticketit::lang.category-name-has-been-created', ['name' => $request->name]));
+
+        \Cache::forget('ticketit::categories');
+
+        return redirect()->action('\Kordy\Ticketit\Controllers\CategoriesController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+        return 'All category related agents here';
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+
+        return view('ticketit::admin.category.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int     $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name'      => 'required',
+            'color'     => 'required',
+        ]);
+
+        $category = Category::findOrFail($id);
+        $category->update(['name' => $request->name, 'color' => $request->color]);
+
+        Session::flash('status', trans('ticketit::lang.category-name-has-been-modified', ['name' => $request->name]));
+
+        \Cache::forget('ticketit::categories');
+
+        return redirect()->action('\Kordy\Ticketit\Controllers\CategoriesController@index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $name = $category->name;
+        $category->delete();
+
+        Session::flash('status', trans('ticketit::lang.category-name-has-been-deleted', ['name' => $name]));
+
+        \Cache::forget('ticketit::categories');
+
+        return redirect()->action('\Kordy\Ticketit\Controllers\CategoriesController@index');
+    }
+}
---
+++ src/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kordy\Ticketit\Middleware;
+
+use Closure;
+use Kordy\Ticketit\Models\Agent;
+use Kordy\Ticketit\Models\Setting;
+
+class IsAdminMiddleware
+{
+    /**
+     * Run the request filter.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Agent::isAdmin()) {
+            return $next($request);
+        }
+
+        return redirect()->route(Setting::grab('main_route') . '.index')
+            ->with('warning', trans('ticketit::lang.you-are-not-permitted-to-access'));
+    }
+}
